9f5952237c21e331cd9a68614841b5e5616f858f
Thomas Loy <ths.loy@googlemail.com>
2015-10-18 14:55:28 +0200

    Enhance with new features

--- src/main/java/com/winthier/reward/Builder.java
+++ src/main/java/com/winthier/reward/Builder.java
@@ -1,17 +1,35 @@
 package com.winthier.reward;
 
+import com.winthier.reward.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class Builder {
-    private final RewardPlugin plugin;
-    private final List<ItemStack> items = new ArrayList<>();
-    private final Reward reward = new Reward();
+    final RewardPlugin plugin;
+    UUID uuid;
+    String name;
+    String comment;
+    double money;
+    int exp;
+    final List<ItemStack> items = new ArrayList<>();
+    final Map<String, Integer> currencies = new HashMap<>();
+    final Map<String, Integer> flags = new HashMap<>();
+    final List<String> commands = new ArrayList<>();
 
     public Builder(RewardPlugin plugin) {
         this.plugin = plugin;
+    }
+
+    public Builder player(Player player) {
+        uuid = player.getUniqueId();
+        name = player.getName();
+        return this;
     }
 
     /**
@@ -19,7 +37,7 @@ public class Builder {
      * calls. You have to call either this or name().
      */
     public Builder uuid(UUID uuid) {
-        reward.setUuid(uuid);
+        this.uuid = uuid;
         return this;
     }
 
@@ -28,7 +46,7 @@ public class Builder {
      * calls. You have to call either this or uuid().
      */
     public Builder name(String name) {
-        reward.setName(name);
+        this.name = name;
         return this;
     }
 
@@ -37,7 +55,7 @@ public class Builder {
      * calls.
      */
     public Builder comment(String comment) {
-        reward.setComment(comment);
+        this.comment = comment;
         return this;
     }
 
@@ -60,28 +78,104 @@ public class Builder {
     }
 
     /**
-     * Only call once. Additional calls will override previous
-     * calls.
+     * Add exp. Call as often as needed.
      */
     public Builder exp(int exp) {
-        reward.setExp(exp);
+        this.exp += exp;
         return this;
     }
 
     /**
-     * Only call once. Additional calls will override previous
-     * calls.
+     * Add money. Call as often as needed.
      */
     public Builder money(double money) {
-        reward.setMoney(money);
+        this.money += money;
+        return this;
+    }
+
+    /**
+     * Add a currency. Call as often as needed. Multiple calls
+     * with the same name will be added up.
+     */
+    public Builder currency(String name, int amount) {
+        Integer value = currencies.get(name);
+        if (value == null) value = 0;
+        value += amount;
+        currencies.put(name, value);
+        return this;
+    }
+
+    /**
+     * Set a flag. Subsequent calls with the same name will
+     * override the previous value.
+     */
+    public Builder flag(String name, int amount) {
+        flags.put(name, amount);
+        return this;
+    }
+
+    /**
+     * Add a command. Replaced will be:
+     * %player% -> name of the player
+     * %uuid% -> uuid of the player
+     */
+    public Builder command(String command) {
+        commands.add(command);
+        return this;
+    }
+
+    /**
+     * Add rewards via configuration section.
+     */
+    public Builder config(ConfigurationSection config) {
+        if (config == null) return this;
+        comment = config.getString("Comment", comment);
+        exp += config.getInt("Exp", 0);
+        money += config.getDouble("Money", 0.0);
+        for (Object o : config.getList("items")) {
+            if (o instanceof ItemStack) {
+                items.add((ItemStack)o);
+            } else if (o instanceof Map) {
+                try {
+                    @SuppressWarnings("unchecked") Map<String, Object> tmp = (Map<String, Object>)o;
+                    ItemStack item = ItemStack.deserialize(tmp);
+                    items.add(item);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        ConfigurationSection section;
+        if (null != (section = config.getConfigurationSection("currencies"))) {
+            for (String key : section.getKeys(false)) {
+                currency(key, section.getInt(key, 0));
+            }
+        }
+        if (null != (section = config.getConfigurationSection("flags"))) {
+            for (String key : section.getKeys(false)) {
+                flag(key, section.getInt(key, 0));
+            }
+        }
+        for (String command : config.getStringList("Commands")) {
+            this.command(command);
+        }
         return this;
     }
 
     public Reward store() {
-        if (reward.getUuid() == null && reward.getName() == null) {
+        if (uuid == null && name == null) {
             throw new RuntimeException("UUID and name can't both be undefined.");
         }
+        Reward reward = new Reward();
+        if (uuid != null) reward.setUuid(uuid);
+        if (name != null) reward.setName(name);
+        if (comment != null) reward.setComment(comment);
+        if (money > 0.01) reward.setMoney(money);
+        if (exp > 0) reward.setExp(exp);
         if (!items.isEmpty()) reward.setItemStacks(items);
+        if (!currencies.isEmpty()) reward.setCurrencyMap(currencies);
+        if (!flags.isEmpty()) reward.setFlagMap(flags);
+        if (!commands.isEmpty()) reward.setCommandList(commands);
         reward.createdNow();
         plugin.getDatabase().save(reward);
         return reward;
--- src/main/java/com/winthier/reward/RewardPlugin.java
+++ src/main/java/com/winthier/reward/RewardPlugin.java
@@ -1,6 +1,8 @@
 package com.winthier.reward;
 
+import com.winthier.reward.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import javax.persistence.OptimisticLockException;
@@ -9,10 +11,6 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandException;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -24,8 +22,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class RewardPlugin extends JavaPlugin implements Listener {
-    private boolean deliver;
-    private Economy economy;
+    boolean deliver;
+    Economy economy;
     public static final String PERM_RECEIVE = "reward.receive";
 
     @Override
@@ -34,40 +32,48 @@ public class RewardPlugin extends JavaPlugin implements Listener {
         reloadConfig();
         configure();
         setupDatabase();
+        getCommand("reward").setExecutor(new RewardCommand(this));
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    private void configure() {
+    void configure() {
         deliver = getConfig().getBoolean("Deliver");
         if (deliver && !setupEconomy()) {
             getLogger().warning("Failed to setup economy");
         }
     }
 
-    private void setupDatabase() {
+    void setupDatabase() {
         try {
             for (Class<?> clazz : getDatabaseClasses()) {
                 getDatabase().find(clazz).findRowCount();
             }
-        } catch (PersistenceException ex) {
+        } catch (PersistenceException tmp) {
             System.out.println("Installing database for " + getDescription().getName() + " due to first time usage");
-            installDDL();
+            try {
+                installDDL();
+            } catch (PersistenceException pe) {
+                pe.printStackTrace();
+            }
         }
     }
 
     @Override
     public List<Class<?>> getDatabaseClasses() {
-        List<Class<?>> list = new ArrayList<Class<?>>();
-        list.add(Reward.class);
-        list.add(Item.class);
-        return list;
+        return Arrays.asList(
+            Reward.class,
+            Item.class,
+            Currency.class,
+            Flag.class,
+            Command.class
+            );
     }
 
     @Override
     public void onDisable() {
     }
 
-    private boolean setupEconomy() {
+    boolean setupEconomy() {
         if (economy != null) return true;
         RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(Economy.class);
         if (economyProvider != null) {
@@ -80,103 +86,6 @@ public class RewardPlugin extends JavaPlugin implements Listener {
         return new Builder(this);
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String args[]) {
-        try {
-            if (false) {
-            } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-                reloadConfig();
-                configure();
-                sender.sendMessage("" + ChatColor.YELLOW + "Configuration reloaded");
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("deliver")) {
-                if (!(sender instanceof Player)) throw new CommandException("Player expected");
-                String idArg = args[1];
-                Player player = (Player)sender;
-                Integer id = null;
-                try { id = Integer.parseInt(idArg); } catch (NumberFormatException nfe) {}
-                if (id == null || id <= 0) throw new CommandException("Positive number expected: " + idArg);
-                Reward reward = getDatabase().find(Reward.class, id);
-                if (reward == null) throw new CommandException("Reward not found: #" + id);
-                deliver(reward, player);
-                sender.sendMessage("" + ChatColor.YELLOW + "Rewards delivered to you");
-            } else if (args.length >= 1 && args[0].equalsIgnoreCase("test")) {
-                if (!(sender instanceof Player)) throw new CommandException("Player expected");
-                // comment
-                String comment = null;
-                if (args.length >= 2) {
-                    StringBuilder sb = new StringBuilder(args[1]);
-                    for (int i = 2; i < args.length; ++i) sb.append(" ").append(args[i]);
-                    comment = sb.toString();
-                }
-                // items
-                List<ItemStack> items = new ArrayList<>();
-                Player player = (Player)sender;
-                for (ItemStack item : player.getInventory()) {
-                    if (item != null && item.getType() != Material.AIR) items.add(item);
-                }
-                Builder builder = createBuilder();
-                builder.uuid(player.getUniqueId());
-                if (comment != null) builder.comment(comment);
-                if (!items.isEmpty()) builder.items(items);
-                builder.exp(player.getTotalExperience());
-                builder.money(12.34);
-                Reward reward = builder.store();
-                sender.sendMessage("" + ChatColor.YELLOW + "Reward stored as #" + reward.getId());
-            } else if (args.length >= 2 && args[0].equalsIgnoreCase("create")) {
-                if (!(sender instanceof Player)) throw new CommandException("Player expected");
-                Player player = (Player)sender;
-                String name = null;
-                Double money = null;
-                Integer exp = null;
-                StringBuilder comment = new StringBuilder();
-                for (int i = 1; i < args.length; ++i) {
-                    String arg = args[i];
-                    if (arg.startsWith("p:")) {
-                        name = arg.substring(2);
-                    } else if (arg.startsWith("m:")) {
-                        String moneyArg = arg.substring(2);
-                        try {
-                            money = Double.parseDouble(moneyArg);
-                        } catch (NumberFormatException nfe) {
-                            throw new CommandException("Money amount expected: " + moneyArg);
-                        }
-                    } else if (arg.startsWith("x:")) {
-                        String expArg = arg.substring(2);
-                        try {
-                            exp = Integer.parseInt(expArg);
-                        } catch (NumberFormatException nfe) {
-                            throw new CommandException("XP amount expected: " + expArg);
-                        }
-                    } else {
-                        comment.append(" ").append(arg);
-                    }
-                }
-                if (name == null) throw new CommandException("Player name required");
-                if (money != null && money < 0.0) throw new CommandException("Money can't be negative");
-                if (exp != null && exp < 0) throw new CommandException("Exp can't be negative");
-                OfflinePlayer recipient = getServer().getOfflinePlayer(name);
-                if (recipient == null || !recipient.hasPlayedBefore()) throw new CommandException("Player not found: " + name);
-                Builder builder = createBuilder();
-                builder.uuid(recipient.getUniqueId());
-                builder.name(recipient.getName());
-                builder.comment(comment.toString().trim());
-                if (exp != null) builder.exp(exp);
-                if (money != null) builder.money(money);
-                for (ItemStack item : player.getInventory()) {
-                    if (item == null || item.getType() == Material.AIR) continue;
-                    builder.item(item.clone());
-                }
-                builder.store();
-                sender.sendMessage("" + ChatColor.AQUA + "Reward for " + recipient.getName() + " stored.");
-            } else {
-                return false;
-            }
-        } catch (CommandException ce) {
-            sender.sendMessage("" + ChatColor.RED + ce.getMessage());
-        }
-        return true;
-    }
-
     /**
      * When a player joins, wait a short time and then give them
      * all their rewards, if any.
@@ -187,10 +96,11 @@ public class RewardPlugin extends JavaPlugin implements Listener {
         final Player player = event.getPlayer();
         new BukkitRunnable() {
             @Override public void run() {
+                if (!player.isValid()) return;
                 if (!player.hasPermission(PERM_RECEIVE)) return;
                 deliverAll(player);
             }
-        }.runTaskLater(this, 20L);
+        }.runTaskLater(this, 20*6);
     }
 
     public void deliverAll(Player player) {
@@ -216,29 +126,51 @@ public class RewardPlugin extends JavaPlugin implements Listener {
         }
     }
 
-    private void deliver(Reward reward, Player player) {
+    void deliver(Reward reward, Player player) {
         // Comment
-        String comment = reward.getComment();
-        if (comment != null) {
-            player.sendMessage("" + ChatColor.AQUA + "You have earned a reward: " + ChatColor.RESET + comment);
-        } else {
-            player.sendMessage("" + ChatColor.AQUA + "You have earned a reward.");
+        player.sendMessage("");
+        player.sendMessage("" + ChatColor.GREEN + ChatColor.BOLD + "Reward");
+        if (reward.getComment() != null) {
+            player.sendMessage(reward.getComment());
         }
         // Exp
-        if (reward.getExp() != null) player.giveExp(reward.getExp());
+        if (reward.getExp() != null && reward.getExp() > 0) {
+            player.sendMessage("Exp: " + ChatColor.GREEN + reward.getExp());
+            player.giveExp(reward.getExp());
+        }
         // Money
-        if (reward.getMoney() != null && economy != null) {
-            EconomyResponse result = economy.depositPlayer(player.getName(), reward.getMoney()); // TODO remove getName() for new Vault version.
+        if (reward.getMoney() != null && reward.getMoney() > 0.01 && economy != null) {
+            player.sendMessage("Money: " + ChatColor.GREEN + economy.format(reward.getMoney()));
+            EconomyResponse result = economy.depositPlayer(player, reward.getMoney());
             if (!result.transactionSuccess()) {
                 getLogger().warning("Failed giving money reward to " + player.getName() + ": " + result.errorMessage);
             }
         }
         // Items
-        for (ItemStack drop : player.getInventory().addItem(reward.getItemStacks()).values()) {
-            if (drop.getType() == Material.AIR) continue;
-            player.getWorld().dropItem(player.getLocation(), drop);
+        ItemStack[] items = reward.getItemStacks();
+        if (items.length > 0) {
+            int amount = 0;
+            for (ItemStack item : items) if (item.getType() != Material.AIR) amount += item.getAmount();
+            for (ItemStack drop : player.getInventory().addItem(items).values()) {
+                if (drop.getType() == Material.AIR) continue;
+                player.getWorld().dropItem(player.getLocation(), drop).setPickupDelay(0);
+            }
+            player.sendMessage("Items: " + ChatColor.GREEN + amount);
+        }
+        player.sendMessage("");
+        // Commands
+        if (reward.getCommands() != null && !reward.getCommands().isEmpty()) {
+            for (Command command : reward.getCommands()) {
+                String cmd = command.getCommand();
+                cmd = cmd.replace("%player%", player.getName());
+                cmd = cmd.replace("%uuid%", player.getUniqueId().toString());
+                getLogger().info("Console command: " + cmd);
+                getServer().dispatchCommand(getServer().getConsoleSender(), cmd);
+            }
         }
         // Log
         getLogger().info("Delivered reward #" + reward.getId() + " to " + player.getName());
+        // Event
+        getServer().getPluginManager().callEvent(new RewardEvent(player, reward));
     }
 }
